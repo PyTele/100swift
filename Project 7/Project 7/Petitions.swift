02b3488f3227e938f7172a58a964e200f9789d09
Hubert Leszkiewicz <64101280+zerotwoswift@users.noreply.github.com>
2020-08-07 19:38:40 +0100

    Add files via upload

---
+++ Project 7/Project 7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by Hubert Leszkiewicz on 30/07/2020.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+    }
