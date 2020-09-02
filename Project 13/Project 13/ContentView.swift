2bf7d88f978c6e54347c24f118648b61d6b157b2
Hubert Leszkiewicz <64101280+zerotwoswift@users.noreply.github.com>
2020-09-02 20:22:14 +0100

    Add files via upload

---
+++ Project 13/Project 13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 13
+//
+//  Created by Hubert Leszkiewicz on 30/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
