355974b23b57866da5708ac0ec300c13de8d23be
Hubert Leszkiewicz <64101280+zerotwoswift@users.noreply.github.com>
2020-10-04 19:45:03 +0100

    Add files via upload

---
+++ Project 19/Project 19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 19
+//
+//  Created by Hubert Leszkiewicz on 28/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
